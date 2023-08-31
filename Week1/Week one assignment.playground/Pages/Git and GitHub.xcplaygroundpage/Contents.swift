1d8824fecdbba3a9fd10f0a9862f9c791c1b26ee
JimmyChao90196 <myname90196@gmail.com>
2023-08-31 15:10:58 +0800

    Assignment part two complete

--- Week1/Week one assignment.playground/Pages/Git and GitHub.xcplaygroundpage/Contents.swift
+++ Week1/Week one assignment.playground/Pages/Git and GitHub.xcplaygroundpage/Contents.swift
@@ -1,29 +1,28 @@
 //AppWorks shchool assignment -week 1
 
 //MARK: - Part one - git and github
-//Here are some Git and GitHub commands we usually use in software development. Please explain the meanings and use cases of them.
 
 
 
 
 //MARK: - git status:
-//The git status command allows us to peek through the status of the current working tree(repository). It displays which files have been altered and are marked as unstaged. Also, it shows the branch we are currently working on.
+//The "git status" command allows us to peek through the status of the current working tree(repository). It displays which files have been altered and are marked as unstaged. Also, it shows the branch we are currently working on.
 
 
 //MARK: - git add:
-//The git add command allow us to stage the modified files. And prepare the files for commit. But there's more to it. There are flags like -i or -p can help us stage files with more accurate controll. For instance -i flage opens a interactive interface that provide various staging(grouping) choices. And -p flag split the changes into individual hunks.
+//The "git add" command allow us to stage the modified files. And prepare the files for commit. But there's more to it. There are flags like -i or -p can help us stage files with more accurate controll. For instance -i flage opens a interactive interface that provide various staging(grouping) choices. And -p flag split the changes into individual hunks.
 
 
 //MARK: - git reset
-//This git reset command can reset not only the staged changes but also commit. Let's say we have three commits A B C, and the head pointer is pointing at commit C. If we use git reset go back to commit A. Then the head pointer will move to commit A, and the commits B and C will revert back to it's original unstaged state.
+//This "git reset" command can reset not only the staged changes but also commit. Let's say we have three commits A B C, and the head pointer is pointing at commit C. If we use git reset go back to commit A. Then the head pointer will move to commit A, and the commits B and C will revert back to it's original unstaged state.
 
 
 //MARK: - git commit
-//To perform git commit, first we have to stage at least one changes. To view those changes at unstaged state, we can use git diff. After the git commit command is excuted. Then the changes is saved at our local repository after we give this commit a short description.
+//To perform "git commit", first we have to stage at least one changes. To view those changes at unstaged state, we can use git diff. After the git commit command is excuted. Then the changes is saved at our local repository after we give this commit a short description.
 
 
 //MARK: - git log
-//Git log let us review the history of commits. And using flags gives us various choices to display this hisotry.
+//"git log" let us review the history of commits. And using flags gives us various choices to display this hisotry.
 // For example:
 // git log -p: Allow us to see the diff of each commits.
 // git log --graph: Graph the history of each commits.
@@ -32,7 +31,7 @@
 
 
 //MARK: - git branch
-//By default git branch will list only local repo branches for us. But if we use it with flags like --all we are able to see remote branches as well. And flag -vv can give us slightly longer description about the branch.
+//By default "git branch" will list only local repo branches for us. But if we use it with flags like --all we are able to see remote branches as well. And flag -vv can give us slightly longer description about the branch.
 
 
 //MARK: - git merge
@@ -47,14 +46,14 @@
 
 
 //MARK: - git push [repo_name] [branch_name]
-//The git commit command is saving changes to local repository, while git push command is saving it to the remote(cloud) repository. We can specify the remote repository and it's branches name as well.
+//The "git commit" command is saving changes to local repository, while "git push" command is saving it to the remote(cloud) repository. We can specify the remote repository and it's branches name as well.
 
 
 //MARK: - git remote
-//Git remote command shows us all the remote repository that are associated with our local repository. We can also use flag -v to see more detail.
+//"git remote" command shows us all the remote repository that are associated with our local repository. We can also use flag -v to see more detail.
 
 
-//MARK: - Fork
+//MARK: - Fork explain
 //The action fork is to copy a repository hosted by someone else under my account. Then we clone the forked repository to our local mechine. Finally if we want, we can add the original repository(the one that was hosted by someone else) to our local repository to get the latest update.
 
 
@@ -76,9 +75,9 @@
 
 
 
-//Step 2-1: Initialize a local repo in terminal by using git init command. That'll make any directory our local repo.
+//Step 2-1: Initialize a local repo in terminal by using "git init" command. That'll make any directory our local repo.
 
-//Step 2-2: Use git remote add [remote name] [url] to associate with remote repo
+//Step 2-2: Use "git remote add [remote name] [url]" to associate with remote repo
 
 //Step 2-3: Then if we haven't add README.md, .gitignore or LICENSE, use vim editor to add it now.
 
---
+++ Week1/Week one assignment.playground/Pages/Swift Basic.xcplaygroundpage/Contents.swift
@@ -0,0 +1,90 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+
+//MARK: - Part two swift Basic
+
+
+
+
+
+//MARK: - 1. The difference between let and var
+// let is a constant. It can not be changed after the declaration.
+// While var stands for variable. The value that store within a variable, can be changed and override any time.
+
+
+//MARK: - 2. Calculate a circumference and store it in a variable
+var radius:Double = 5
+let pi:Double = Double.pi
+let circumference = 2 * pi * radius
+
+print(circumference)
+
+
+
+//MARK: - 3. Average calculation
+let x = 10
+let y = 6
+
+let average = (x + y) / 2
+
+
+
+//MARK: - 4-1. Convert Int to Double
+let recordSystem:Double = Double(average)
+
+
+
+//MARK: - 4-2. The different between 10/3 and 10.0/3.0
+var integerDivision = (10/3) // This will return a rounded integer to us
+var doubleDivision = (10.0/3.0) // And this will give us a double
+
+
+
+//MARK: - 5. Remainder calculation
+let numA = 10
+let numB = 3
+
+let remainder = 10%3
+
+
+
+//MARK: -6. Specify data type manually
+var flag:Bool = true
+var inputString:String = "Hello world"
+let bitsInBite:Int = 8
+let averageScore:Double = 86.8
+
+
+
+//MARK: -7. Type inference explaination
+//Type inference means swift can determine and guess the data type based on the input value we provided. We don't have to manually decide the data type.
+
+
+
+//MARK: -8. Type mis-match
+var phoneNumber = 0987654321
+phoneNumber = "Hello world"
+
+//The code above is likely to get an error of type mis-match. The first line we declare a phoneNumber with the value of 0987654321. Without specify the type of the phoneNumber variable. Type inference will then decide the type according to the input value, which is integer.
+
+//So when we want to override the value with a string, swift will then complain loudly.
+
+
+
+//MARK: -9. Compound operator
+var salary = 22000
+salary += 28000
+
+
+
+//MARK: -10. Usage of equal operator
+var target = 10
+
+if target == 10{
+    print("Awesome, that's a perfect 10")
+}
+
+//Equal operator in swift is written as "==", we can also call it a compare operator.
+//Same as >,<,<=,>= ... they are all compare operator
